1a860e33ed171c0439deaf5548b7c057a23b6887
Derek Mercedes <mercedes.derek@gmail.com>
2023-10-29 16:05:01 -0600

    makes if checks

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
+
+  if(num <= 400){
+    return "This one is on me!"
+  } else if (num > 400 && num <= 2000){
+    return "That will be twenty bucks."
+  } else if (num > 2000 && num <= 2500){
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
+
+}
+
+function ternaryCheckCity(city){
+
+  if(city === "NYC"){
+    return "Ok, sounds good."
+  } else if(city !== "NYC"){
+    return "No go."
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  if(tip === 'generous'){
+    return "Thank you so much."
+  } else if (tip === 'not as generous'){
+    return "Thank you."
+  } else {
+    return "Bye."
+  }
 }
